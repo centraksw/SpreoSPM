649dd7d0ed5c3408e9feeb54d5f2414b2a615e03
Alexander Lozovoy <a.lozovoy@belitsoft.com>
2022-07-07 11:56:10 +0300

    local frameworks added

--- Package.swift
+++ Package.swift
@@ -17,18 +17,9 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "IndoorKit", path: "IndoorKit.xcframework"),
-        .binaryTarget(name: "GoogleMaps",
-                      url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/6.0.0/GoogleMaps.xcframework.zip",
-                      checksum: "cd775df376017bf507d3f0bdfc29a60ebbe35f58a44333a54a7786c07c546036"
-        ),
-        .binaryTarget(name: "GoogleMapsBase",
-                      url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/6.0.0/GoogleMapsBase.xcframework.zip",
-                      checksum: "37bbaf7a16c9495a791166f2ea64835bd8506034deb2f0970ec2c70b98cb310f"
-        ),
-        .binaryTarget(name: "GoogleMapsCore",
-                      url: "https://github.com/YAtechnologies/GoogleMaps-SP/releases/download/6.0.0/GoogleMapsCore.xcframework.zip",
-                      checksum: "3c628402426869708f2b160e51526b5088e8d7a398b454ec7cba2ba11729cdb2"
-        ),
+        .binaryTarget(name: "GoogleMaps", path: "GoogleMaps.xcframework"),
+        .binaryTarget(name: "GoogleMapsBase", path: "GoogleMapsBase.xcframework"),
+        .binaryTarget(name: "GoogleMapsCore", path: "GoogleMapsCore.xcframework"),
         .target(name: "SpreoSMP",
                 dependencies: [
                     .target(name: "IndoorKit", condition: .when(platforms: .some([.iOS]))),
@@ -37,7 +28,8 @@ let package = Package(
                     .target(name: "GoogleMapsCore", condition: .when(platforms: .some([.iOS])))
                 ],
                 path: "Sources/SpreoSPM",
-                resources: [.copy("IndoorKit.bundle")]
+                resources: [.copy("IndoorKit.bundle"),
+                            .copy("GoogleMaps.bundle")]
         )
     ]
 )
