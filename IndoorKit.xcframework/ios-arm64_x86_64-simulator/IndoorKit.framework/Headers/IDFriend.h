b4be95d1f7c04c228061939926140b0b7491293f
Alexander Lozovoy <a.lozovoy@belitsoft.com>
2022-09-13 12:12:33 +0300

    new release 6.9.16

---
+++ IndoorKit.xcframework/ios-arm64_x86_64-simulator/IndoorKit.framework/Headers/IDFriend.h
@@ -0,0 +1,31 @@
+//
+//  IDFriend.h
+//  IndoorKit
+//
+//  Created by Spreo on 2/26/18.
+//  Copyright © 2018 Spreo LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@class IDLocation;
+@class GMSMarker;
+
+@interface IDFriend : NSObject 
+
+@property (nonatomic, readonly, copy) NSString *id;
+@property (nonatomic, readonly, copy) UIImage *icon;
+@property (nonatomic, strong, readonly) NSString *name;
+@property (nonatomic) IDLocation *location;
+@property (nonatomic) GMSMarker *marker; // Use Utility Class ID Gms Class
+
+- (instancetype)initFriend:(NSString *)id
+                        icon:(UIImage *)icon
+                     name:(NSString *)name
+                    location:(IDLocation *)aLocation;
+
+@end
+
+ 
+
--- Package.swift
+++ Package.swift
@@ -16,9 +16,7 @@ let package = Package(
         )
     ],
     targets: [
-        .binaryTarget(name: "IndoorKit",
-                      url: "https://github.com/SpreoLLC/SpreoSPM/releases/download/6.9.12/IndoorKit.xcframework.zip",
-                      checksum: "1635acb041f23669794ae3ef653c98d144f47151b8c7e9e810158d4f6d3b9188"),
+        .binaryTarget(name: "IndoorKit", path: "IndoorKit.xcframework"),
         .target(name: "SpreoSMP",
                 dependencies: [
                     .target(name: "IndoorKit", condition: .when(platforms: .some([.iOS])))
